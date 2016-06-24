7864fb525bd4ac657e440dc3e15777a6125b5923
Harm van Beek <harm@the-incredible-machine.com>
2016-06-24 14:57:30 +0100

    Added some beeping functionlaity, the higher the beep the more moist...

--- Arduino/AgrigatorReceiver/AgrigatorReceiver.ino
+++ Arduino/AgrigatorReceiver/AgrigatorReceiver.ino
@@ -1,6 +1,8 @@
 int inByte = 0;
 int ledPin = 6;
 
+int pitches[] = {784, 880, 1047, 1319, 1568};
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -16,11 +18,16 @@ void blinkLED(){
   delay(200);
 }
 
+void playTone(char inByte){
+  tone(4, pitches[(int) inByte-48], 500);
+ }
+
 void loop() {
    
    if (Serial1.available() > 0) {
-    byte inByte = Serial1.read();
+    char inByte = Serial1.read();
     Serial.println(inByte); //DEBUG
+    playTone(inByte);
     blinkLED();
     delay(20);
    }
